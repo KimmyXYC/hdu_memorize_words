3e1683c24dd62331169648256efb9328de629dd1
yeyeye613 <1393250881@qq.com>
2025-04-04 21:27:17 +0800

    优化了多用户使用

--- OpenWeb.py
+++ OpenWeb.py
@@ -1,194 +1,150 @@
 # -*- coding: utf-8 -*-
-import re  # 处理正则表达式
 import time
-from selenium.webdriver.support import expected_conditions as EC  # 提供了一组预定义的条件，
+
+from selenium.common import NoSuchElementException
+from selenium.webdriver.support import expected_conditions as ec  # 提供了一组预定义的条件，
 from selenium.webdriver.support.wait import WebDriverWait  # 上通常与WebDriverWait一起使用，用于等待某个特定条件成立后再继续执行代码
 from selenium import webdriver
 from selenium.webdriver.common.by import By  # 定位页面元素的方法
+from selenium.webdriver.chrome.service import Service
 import os
 import json
+import re
 
 
-def print_table():
-    """打印用户界面"""
-    # 数据
-    data = [
-        ["序号", "选项"],
-        ["1", "开始登录"],
-        ["2", "用户设置"]
-    ]
+class HDU:
+    def __init__(self):
+        options = webdriver.ChromeOptions()
+        # 开启手机模式
+        options.add_experimental_option('mobileEmulation', {'deviceName': 'iPhone X'})  # 开启手机模式
+        # 驱动路径
+        chrome_driver_path = r"C:\Users\yyy\.cache\selenium\chromedriver\win64\chromedriver-win64\chromedriver.exe"
 
-    # 计算每列的最大宽度
-    col_widths = [max(len(str(cell)) for cell in column) for column in zip(*data)]
+        service = Service(executable_path=chrome_driver_path)  # , service=service
 
-    # 绘制表格
-    print("+" + "-+-" * len(data[0]) + "+")
-    for row in data:
-        print("|", end="")
-        for i, cell in enumerate(row):
-            print(" " + str(cell).center(col_widths[i]) + " |", end="")
-        print()
-    print("+" + "-+-" * len(data[0]) + "+")
+        self.driver = webdriver.Chrome(options=options, service=service)  # 实例化ChromeDriver对象
 
+        # 加载tm。json
+        try:
+            with open("题库.json", 'r', encoding='utf-8') as file:
+                self.answer = json.load(file)
+        except FileNotFoundError:
+            print(f"错误: 文件未找到。")
+        except json.JSONDecodeError:
+            print(f"错误: 文件不是有效的JSON格式。")
+        except Exception as e:
+            print(f"错误: 发生未知错误: {e}")
 
-def write_user():
-    username = input("输入账号")
-    password = input("输入密码")
-    line = username + " " + password
-    with open("data.json", "a", encoding='utf-8') as f:
-        f.write(line)
+    def login(self):
+        if os.path.exists("data.json"):
+            with open("data.json", "r", encoding='utf-8') as f:
+                data = json.load(f)
+                print(data)
+                # 缺一段新建用户的代码
+                inp = input("请输入你想登录的账号")
+                username = data[inp]["username"]
+                password = data[inp]["password"]
+        else:
+            # 新建账号密码文件
+            username = input(" 请输入用户名：")
+            password = input(" 请输入密码：")
+            addition = input(" 请输入备注：")
+            new_user = {1: {"username": username, "password": password, "addition": addition}}
+            with open("data.json", "w", encoding='utf-8') as f:
+                json.dump(new_user, f, ensure_ascii=False)
+        self.loginweb(username, password)
 
-
-def read_browser_path_from_config():
-    """从配置文件中读取浏览器路径"""
-
-    config_file_path = "config.txt"  # 定义配置文件的路径 config_file_path 并设置为 "config.txt"。
-    if os.path.exists(config_file_path):  # 使用 os.path.exists 函数检查配置文件是否存在。
-        with open(config_file_path, 'r') as file:  # 如果文件存在，使用 with open 语句以只读模式打开文件，并读取所有行到 lines 变量中
-            lines = file.readlines()
-            for line in lines:
-                if line.startswith("browser_path:"):
-                    return line.split("browser_path:")[1].strip()
-                # 假设配置文件 config.txt 中有一行是以 "browser_path:" 开头的，后面跟着浏览器可执行文件的路径。例如：
-                # browser_path: C:\Program Files\Google\Chrome\Application\chrome.exe
-    return None
-
-
-def login():
-    with open('data.json', 'r', encoding='utf-8') as file:
-        loaded_data = json.load(file)
-        n = 1
-        for i in loaded_data:
-            print(n, loaded_data[i][0])
-            n += 1
-        action = input()
-        username = loaded_data[action][1]
-        password = loaded_data[action][2]
-    radio_button_selector = (
-        "#app > div > div.tn-body__container > div > div.van-radio-group > "
-        "div > div:nth-child(1) > div.van-cell__title > span"
-    )
-
-
-    # 打开网页并登录
-    url = "https://skl.hduhelp.com/#/english/list"
-    driver.get(url)
-
-    WebDriverWait(driver, 10).until(
-        EC.element_to_be_clickable((By.CSS_SELECTOR, '.login-button'))
-    )
-    # if action == '0':
-    #     write_user()
-    # else:
-    username_input = driver.find_element(By.NAME, 'username')
-    username_input.send_keys(username)
-
-    password_input = driver.find_element(By.CSS_SELECTOR, '.ant-input[type="password"]')  # 通过CSS选择器定位密码框
-    password_input.send_keys(password)  # 输入密码
-
-    login_button = driver.find_element(By.CSS_SELECTOR, '.login-button')  # 使用CSS选择器定位登录按钮
-    login_button.click()  # 点击登录按钮
-
-
-def tn_answer(tn_text, answer_option="tm找不到"):
-    # 使用正则表达式提取问题和选项
-    global question, found, i, radio_button_selector
-    question_match = re.search(r'(\d+)\s*(.+?)\s*\.', tn_text)
-    options_match = re.findall(r'([A-D])\.\s*(.+)', tn_text)
-
-    if question_match:
-        question = question_match.group(2).strip()
-        for i in range(len(question)):
-            if 122 >= ord(question[i]) >= 65 or '\u4e00' <= question[i] <= '\u9fff':
-                break
-        question = question[i:]
-        if "，" in question:
-            question = question.replace("，", " ")
-
-    options = {}
-    if options_match:
-        for match in options_match:
-            options[match[0]] = match[1].strip()
-
-    with open('四级.txt', 'r', encoding='utf-8') as answer_file:
-        tn_lines = answer_file.readlines()
-
-    for line in tn_lines:
-        found = False
-        for ABCD in ["A", "B", "C", "D"]:
-            if question in line and options[ABCD][:-2] in line:
-                answer_option = ABCD
-                found = True
-                break
-
-        if found:
-            if answer_option == "A":
-                radio_button_selector = (
-                    "#app > div > div.tn-body__container > div > div.van-radio-group > "
-                    "div > div:nth-child(1) > div.van-cell__title > span"
-                )
-
-            if answer_option == "B":
-                radio_button_selector = (
-                    "#app > div > div.tn-body__container > div > div.van-radio-group > "
-                    "div > div:nth-child(2) > div.van-cell__title > span"
-                )
-            if answer_option == "C":
-                radio_button_selector = (
-                    "#app > div > div.tn-body__container > div > div.van-radio-group > "
-                    "div > div:nth-child(3) > div.van-cell__title > span"
-                )
-            if answer_option == "D":
-                radio_button_selector = (
-                    "#app > div > div.tn-body__container > div > div.van-radio-group > "
-                    "div > div:nth-child(4) > div.van-cell__title > span"
-                )
-
-            radio_button = WebDriverWait(driver, 10).until(
-                EC.element_to_be_clickable((By.CSS_SELECTOR, radio_button_selector))
-            )
-            # 点击定位到的单选按钮
-            radio_button.click()
-            # print(f"答案为{answer_option}")
-
-            break
-    if not found:
-        # button = driver.find_element(By.XPATH, '//*[@id="app"]/div/div[3]/div/div[5]/div[1]/div[3]/button')
-        button = driver.find_element(By.CSS_SELECTOR,
-            "#app > div > div.tn-body__container > div > div.van-radio-group > "
-            "div > div:nth-child(3) > div.van-cell__title > span"
+    def loginweb(self, username, password):
+        url = "https://skl.hduhelp.com/#/english/list"  # 网址
+        # 打开网页
+        self.driver.get(url)
+        WebDriverWait(self.driver, 10).until(
+            ec.element_to_be_clickable((By.CSS_SELECTOR, '.login-button'))
         )
-        button.click()
+        # 输入用户名密码
+        self.driver.find_element(By.NAME, "username").send_keys(username)
+        self.driver.find_element(By.NAME, "passwordPre").send_keys(password)
+        self.driver.find_element(By.CSS_SELECTOR, "button[type='submit']").click()
+        input("请手动开始考试后按回车继续")
+    #加个选自测和考试的跳转
 
-web_options = webdriver.ChromeOptions()
-web_options.add_experimental_option('mobileEmulation', {'deviceName': 'iPhone X'})
-driver = webdriver.Chrome(options=web_options)
-print_table()
-i = int(input("请输入命令"))
-if i == 1:
-    login()
-    with open('四级.txt', 'r', encoding='utf-8') as file:
-        tn_lines = file.readlines()
-    input("已读取题库,回车给出当前页面答案")
-    for i in range(100):
-        script = "return document.body.innerText;"
-        text = driver.execute_script(script)
-        tn_answer(text)
-        time.sleep(1)
-        # WebDriverWait(driver, 10).until(
-        # EC.element_to_be_clickable((By.CLASS_NAME, "van-cell van-cell--clickable"))
-        # )
-    print("已经作答完毕大部分题目,可以点击提交了")
-    for i in range(300, -1, -1):
-        # 使用 '\r' 返回行首，不换行，'end='' 确保不追加新行
-        print(f"\r剩余时间: {i} 秒", end='', flush=True)
-        time.sleep(1) 
-    submit_button = driver.find_element(By.XPATH, '//*[@id="app"]/div/div[2]/div/div/div[3]/span')
-    time.sleep(2)
-    submit_button.click()
-    confirm_button = driver.find_element(By.XPATH, '/html/body/div[4]/div[2]/button[2]')
-    confirm_button.click()
-    # driver.quit() # 下一题按钮Xpath://*[@id="app"]/div/div[3]/div/div[5]/div[1]/div[3]/button
-    # /html/body/div[4]/div[2]/button[2]
-    input("1111")
-    driver.quit()
+    def find_question(self) -> tuple[str, list[str]]:
+        # 获取题目单词
+        span_elements = self.driver.find_element(By.CLASS_NAME, "van-col--17").find_elements(By.TAG_NAME, "span")
+        question = span_elements[1].text.strip()[:-2]  # 去掉最后的标点符号
+
+        # 获取选项
+        options = self.driver.find_elements(By.CLASS_NAME, "van-cell__title")
+        options_list = [re.sub(r'\s.', '', opt.text[3:]) for opt in options[:4]]
+        print(f"\n{question}\n"
+              f"A: {options_list[0]}\n"
+              f"B：{options_list[1]}\n"
+              f"C：{options_list[2]}\n"
+              f"D：{options_list[3]}\n")
+
+        return question, options_list
+
+    def find_answer(self, question_options: tuple[str, list[str]]) -> int:
+        question, options_list = question_options
+        for i in range(len(options_list)):
+            try:
+                if self.answer[question] == options_list[i]:
+                    return i
+            except KeyError:
+                self.SaveError(question_options)
+                return -1
+        self.SaveError(question_options)
+        return -1
+
+    def click_answer(self, index: int) -> None:
+        if index == -1:
+            next_button = self.driver.find_element(
+                By.XPATH, '//*[@id="app"]/div/div[3]/div/div[5]/div[1]/div[3]/button'
+            )
+            next_button.click()
+        else:
+            options = self.driver.find_elements(By.CLASS_NAME, "van-cell__title")
+            print(index)
+            options[index].click()
+
+    @staticmethod
+    def SaveError(question_options):
+        question, options = question_options
+        error_message = f"{question}\n{options}\n"
+        with open("error.txt", "a", encoding='utf-8') as f:
+            f.write(error_message)
+
+    def wait(self):
+        # 等待答题时间
+        print("已经作答完毕大部分题目,可以点击提交了")
+        print("错误题目已保存到error.txt")
+        for i in range(300, -1, -1):
+            # 使用 '\r' 返回行首，不换行，'end='' 确保不追加新行
+            print(f"\r剩余时间: {i} 秒，时间到后将自动提交", end='', flush=True)
+            time.sleep(1)
+        try:
+            submit_btn = self.driver.find_element(By.CLASS_NAME, "van-nav-bar__right")
+            submit_btn.click()
+            time.sleep(1)
+            check_btn = self.driver.find_element(By.CSS_SELECTOR, ".van-dialog__confirm.van-hairline--left")
+            check_btn.click()
+            time.sleep(1)
+            print("\n")
+        except Exception as e:
+            print("靠(艹皿艹 )，程序又崩了，快自己点击救一下啊！")
+
+    def start(self):
+        self.login()
+        for i in range(100):
+            question_options = self.find_question()
+            answer_index = self.find_answer(question_options)
+            self.click_answer(answer_index)
+            time.sleep(1)
+        self.wait()
+        time.sleep(5)
+        self.driver.quit()
+
+
+if __name__ == "__main__":
+    hdu = HDU()
+    hdu.start()
